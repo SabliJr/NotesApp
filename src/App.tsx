bc3b365c171aa4705a7e13a2e832546b98f44ebd
Sabli Jr <sablijr@yahoo.com>
2023-03-23 21:08:05 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.css";
+
+const App = () => {
+  return (
+    <div className='App'>
+      <h2>Hello Ts</h2>
+    </div>
+  );
+};
+
+export default App;
